68f6f339cb3835ba8f2397bcdbd67fb162ad093b
Swapnil Mishra <swapmis1084@gmail.com>
2014-04-16 00:16:05 +0530

    first commit.

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ cache-javascript-localstorage
 =============================
 
 Simple utility micro library to fetch and store javascript in localStorage to use it for next time.
+
+#### Usage
+
+Requires jQuery as dependency for script fetch call.
+
+To use it call 
+
+```javascript
+  fclocalstorage.require(url,key)
+```
+
+key is the key name by which you want to store it
---
+++ fclocalstorage.js
@@ -0,0 +1,51 @@
+(function(window,document,localStorage){
+
+  var head = document.head || document.getElementsByTagName('head')[0];
+
+  var require = function(url,key){
+    fetchScript(url,key);
+  }
+
+  var fetchScript = function(url,key){
+    var scriptData = getLocalStoreData(key);
+    if(scriptData){
+      injectScript(scriptData);
+      console.log('script found in local Storage');
+    }
+    else{
+      if(!url){
+        console.log("Please supply a url to be fetched");
+      }
+      else {
+        console.log("Fetching script");
+        $.getScript(url)
+        .done(function(data,status){
+          setLocalStoreData(key,data);
+        });
+      }
+    }
+  }
+
+  var setLocalStoreData = function(key,data){
+    var stringifyData  = JSON.stringify(data);
+    localStorage.setItem(key,stringifyData);
+  }
+
+  var injectScript = function( obj ) {
+    var script = document.createElement('script');
+    script.defer = true;
+    script.text = obj;
+    head.appendChild( script );
+  };
+
+  var getLocalStoreData = function(key){
+    var data = localStorage.getItem(key);
+    return JSON.parse(data);
+    // injectScript( JSON.parse(data) );
+  }
+
+  window.fclocalstorage  = {
+    require       : require,
+    getLocalStoreData : getLocalStoreData
+  };
+})(window,document,localStorage);
